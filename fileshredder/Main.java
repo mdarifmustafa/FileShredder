b75b980dcf3cc7a3f0367d31050052181f88db9d
Martino Perin <perinmartino@gmail.com>
2015-10-26 15:12:22 +0100

    Starring JFileChooser!

--- fileshredder/Main.java
+++ fileshredder/Main.java
@@ -2,20 +2,23 @@ package fileshredder;
 
 import java.io.IOException;
 import javax.swing.JOptionPane;
+import javax.swing.JFileChooser;
 import java.util.*;
 
 public class Main {
 	public static void main(String []args) throws IOException {
-	// Scanner for cli version
-	//Scanner input = new Scanner(System.in);
-  	String tmp = JOptionPane.showInputDialog("File path");
+		// Scanner for cli version
+		//Scanner input = new Scanner(System.in);
+  	JFileChooser chooser = new JFileChooser();
+  	chooser.showOpenDialog(null);
+  	String tmp = chooser.getSelectedFile().getPath();
   	// Check if the string is null to avoid NullPointerException
   	if(tmp != null){
   		ToRemove t = new ToRemove();
   		t.secureDelete(tmp);
+		}
+		else{
+			System.exit(1);
+		}
 	}
-	else{
-		System.exit(1);
-	}
-}
 }
